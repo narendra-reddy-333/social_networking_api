d0b5ff5e7a8f87261a7067322d0d119d673e3d1e
narendra <dnarendrareddy1997@gmail.com>
2024-05-09 12:46:28 +0530

    added comments to code

--- friends/views.py
+++ friends/views.py
@@ -1,12 +1,9 @@
-# Create your views here.
-# friends/views.py
 from django.db import transaction
 from django.db.models import Q
 from rest_framework import generics, status
 from rest_framework.exceptions import Throttled, ValidationError, PermissionDenied
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-
 from users.serializers import UserSerializer
 from .models import FriendRequest, User
 from .serializers import FriendRequestSerializer
@@ -14,16 +11,23 @@ from .throttling import SendFriendRequestRateThrottle
 
 
 class FriendRequestView(generics.CreateAPIView, generics.ListAPIView):
+    """
+    View for creating and listing friend requests.
+    """
     serializer_class = FriendRequestSerializer
-    throttle_classes = [SendFriendRequestRateThrottle]
+    throttle_classes = [SendFriendRequestRateThrottle]  # Apply rate limiting to prevent spam
 
     def create(self, request, *args, **kwargs):
+        """
+        Handles creation of a new friend request.
+        """
         try:
             receiver_email = request.data.get('receiver_email')
             if not receiver_email:
                 raise ValidationError({"receiver_email": "This field is required."})
-
             receiver = get_object_or_404(User, email=receiver_email)
+
+            # Perform validations before creating the friend request
             self.check_self_request(request.user, receiver)
             self.check_existing_friend_requests(request.user, receiver)
             self.check_existing_friends(request.user, receiver)
@@ -32,16 +36,22 @@ class FriendRequestView(generics.CreateAPIView, generics.ListAPIView):
             serializer = self.get_serializer(friend_request)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Throttled as e:
-            # Call handle_throttled to handle rate limit errors
+            # Handle rate limit errors
             return self.handle_throttled(request, e.wait)
 
     @staticmethod
     def check_self_request(sender, receiver):
+        """
+        Checks if the user is trying to send a friend request to themselves.
+        """
         if sender == receiver:
             raise ValidationError({"error": "You cannot send a friend request to yourself"})
 
     @staticmethod
     def check_existing_friend_requests(sender, receiver):
+        """
+        Checks if a friend request already exists between the users.
+        """
         if FriendRequest.objects.filter(
                 Q(sender=sender, receiver=receiver, status='pending') |
                 Q(sender=receiver, receiver=sender, status='pending')
@@ -50,6 +60,9 @@ class FriendRequestView(generics.CreateAPIView, generics.ListAPIView):
 
     @staticmethod
     def check_existing_friends(sender, receiver):
+        """
+        Checks if the users are already friends.
+        """
         if FriendRequest.objects.filter(
                 Q(sender=sender, receiver=receiver, status='accepted') |
                 Q(sender=receiver, receiver=sender, status='accepted')
@@ -57,24 +70,38 @@ class FriendRequestView(generics.CreateAPIView, generics.ListAPIView):
             raise ValidationError({"error": "Both are already friends."})
 
     def handle_throttled(self, request, wait):
-        # Customize throttled response, if needed
+        """
+        Handles rate limit exceeded errors.
+        """
         raise Throttled(detail={"error": "Rate limit exceeded. Please try again later."}, wait=wait)
 
     def get_queryset(self):
+        """
+        Returns a list of pending friend requests received by the user.
+        """
         return self.request.user.received_requests.filter(status='pending')
 
 
 class AcceptRejectFriendRequestView(generics.UpdateAPIView):
+    """
+    View for accepting or rejecting friend requests.
+    """
     serializer_class = FriendRequestSerializer
 
     def get_object(self):
+        """
+        Retrieves the specific FriendRequest object based on the request_id.
+        """
         request_id = self.kwargs['request_id']
         obj = get_object_or_404(FriendRequest, pk=request_id, status='pending')
-        self.check_object_permissions(self.request, obj)
+        self.check_object_permissions(self.request, obj)  # Ensure only the receiver can act
         return obj
 
     @transaction.atomic()
     def update(self, request, *args, **kwargs):
+        """
+        Updates the status of the friend request to 'accepted' or 'rejected'.
+        """
         instance = self.get_object()
         _status = request.data.get('status')
         if _status not in ['accepted', 'rejected']:
@@ -85,14 +112,23 @@ class AcceptRejectFriendRequestView(generics.UpdateAPIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def check_object_permissions(self, request, obj):
+        """
+        Checks if the current user is the receiver of the friend request.
+        """
         if request.user != obj.receiver:
             raise PermissionDenied(detail={"error": "You do not have permission to accept friend request."})
 
 
 class ListFriendsView(generics.ListAPIView):
+    """
+    View for listing the user's friends.
+    """
     serializer_class = UserSerializer
 
     def get_queryset(self):
+        """
+        Retrieves a list of the user's friends based on accepted friend requests.
+        """
         friends = User.objects.filter(
             Q(sent_requests__receiver=self.request.user, sent_requests__status='accepted') |
             Q(received_requests__sender=self.request.user, received_requests__status='accepted')
--- users/views.py
+++ users/views.py
@@ -1,39 +1,51 @@
-# Create your views here.
-
 from django.db.models import Q
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from .models import User
 from .serializers import UserSerializer
 
 
 class UserRegistrationView(generics.CreateAPIView):
+    """
+    View for registering new users.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.AllowAny,)  # Allow any user to access
+    permission_classes = (permissions.AllowAny,)  # Allow any user (even unauthenticated) to access
 
     def create(self, request, *args, **kwargs):
+        """
+        Handles user registration.
+        """
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
+        serializer.is_valid(raise_exception=True)  # Raise an exception if data is invalid
+        user = serializer.save()  # Save the new user
         return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
 
 
 class UserLoginView(TokenObtainPairView):
+    """
+    View for user login and JWT token generation.
+    """
     permission_classes = (permissions.AllowAny,)  # Allow any user to access
 
-    # No need to override post() method.
-    # TokenObtainPairView handles token generation and response.
+    # No need to override post() method as TokenObtainPairView handles 
+    # token generation and response based on credentials.
 
 
 class UserSearchView(generics.ListAPIView):
+    """
+    View for searching users by email or name.
+    """
     serializer_class = UserSerializer
 
     def get_queryset(self):
+        """
+        Filters users based on the provided query parameter.
+        """
         query = self.request.query_params.get('query')
-        print(query)
         if query:
+            # Search for users with matching email (case-insensitive) or name containing the query
             return User.objects.filter(Q(email__iexact=query) | Q(name__icontains=query))
-        return User.objects.none()  # Return empty queryset if no query is provided
+        return User.objects.none()  # Return an empty queryset if no query is provided
